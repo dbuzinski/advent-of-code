aedc0682034f56959eb5e9c8dfac1449f5aeab6a
David Buzinski <davidbuzinski@gmail.com>
2023-12-04 01:24:55 -0500

    day 3 late and in a rush. pls don't judge :)

---
+++ 2023/rust/src/bin/03-1.rs
@@ -0,0 +1,113 @@
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader, Lines };
+
+fn main() {
+    let filename = "data/03.txt";
+
+    let lines = read_lines(filename).unwrap();
+
+    let mut sum: usize= 0;
+
+    let mut sc0: Option<Vec<usize>>;
+    let mut sc1: Option<Vec<usize>> = None;
+    let mut sc2: Option<Vec<usize>> = None;
+    let mut ip1: Vec<(usize, usize)>;
+    let mut ip2: Vec<(usize, usize)> = vec![];
+    let mut prev_line: String = String::from("");
+    for line in lines {
+        if let Ok(txt) = line {
+            let (int_pos, sc_pos) = parse_line(&txt);
+            sc0 = sc1;
+            sc1 = sc2;
+            sc2 = Some(sc_pos);
+            ip1 = ip2;
+            ip2 = int_pos;
+            for part_pos in get_parts(&ip1, &sc0, &sc1, &sc2).iter() {
+                let part = prev_line.get(part_pos.0..part_pos.1).unwrap().parse::<usize>().unwrap();
+                sum = sum + part;
+            }
+            prev_line = txt.to_string();
+        }
+    }
+    sc0 = sc1;
+    sc1 = sc2;
+    sc2 = None;
+    for part_pos in get_parts(&ip2, &sc0, &sc1, &sc2).iter() {
+        let part = prev_line.get(part_pos.0..part_pos.1).unwrap().parse::<usize>().unwrap();
+        sum = sum + part;
+    }
+    println!("{}", sum);
+}
+
+fn parse_line(line: &str) -> (Vec<(usize, usize)>, Vec<usize>) {
+    let mut in_int = false;
+    let mut int_start: usize = 0;
+    let mut int_end: usize;
+    let mut sc_pos: Vec<usize> = vec![];
+    let mut int_pos: Vec<(usize, usize)> = vec![];
+    for c in line.char_indices() {
+        match c.1 {
+            '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => {
+                if !in_int {
+                    in_int = true;
+                    int_start = c.0;
+                }
+            }
+            '.' => {
+                if in_int {
+                    int_end = c.0;
+                    in_int = false;
+                    int_pos.push((int_start, int_end));
+                }
+            },
+            _ => {
+                if in_int {
+                    int_end = c.0;
+                    in_int = false;
+                    int_pos.push((int_start, int_end));
+                }
+                sc_pos.push(c.0);
+            }
+        }
+    };
+    if in_int {
+        int_end = line.len();
+        int_pos.push((int_start, int_end));
+    }
+    (int_pos, sc_pos)
+}
+
+
+fn get_parts<'a>(int_pos: &Vec<(usize, usize)>, sc0: &'a Option<Vec<usize>>, sc1: &'a Option<Vec<usize>>, sc2: &'a Option<Vec<usize>>) -> Vec<(usize, usize)> {
+    let mut parts: Vec<(usize, usize)> = vec![];
+    for ip in int_pos.iter() {
+        if is_part(*ip, sc0, sc1, sc2) {
+            parts.push(*ip);
+        }
+    }
+    parts
+}
+
+fn is_part(int_pos: (usize, usize), sc0: &Option<Vec<usize>>, sc1: &Option<Vec<usize>>, sc2: &Option<Vec<usize>>) -> bool {
+    for scp0 in sc0.clone().unwrap_or_default().into_iter() {
+        if int_pos.0.checked_sub(1).unwrap_or_default() <= scp0 && scp0 <= int_pos.1 {
+            return true
+        }
+    }
+    for scp1 in sc1.clone().unwrap_or_default().into_iter() {
+        if int_pos.0.checked_sub(1).unwrap_or_default() <= scp1 && scp1 <= int_pos.1 {
+            return true
+        }
+    }
+    for scp2 in sc2.clone().unwrap_or_default().into_iter() {
+        if int_pos.0.checked_sub(1).unwrap_or_default() <= scp2 && scp2 <= int_pos.1 {
+            return true
+        }
+    }
+    false
+}
+
+fn read_lines(filename: &str) -> io::Result<Lines<BufReader<File>>> {
+    let f = File::open(filename)?;
+    Ok(BufReader::new(f).lines())
+}
---
+++ 2023/rust/src/bin/03-2.rs
@@ -0,0 +1,127 @@
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader, Lines };
+
+fn main() {
+    let filename = "data/03.txt";
+
+    let lines = read_lines(filename).unwrap();
+
+    let mut sum: usize= 0;
+
+    let mut ints0: Option<Vec<(usize, usize)>>;
+    let mut ints1: Option<Vec<(usize, usize)>> = None;
+    let mut ints2: Option<Vec<(usize, usize)>> = None;
+    let mut line0: String;
+    let mut line1: String = String::from("");
+    let mut line2: String = String::from("");
+    let mut gears1: Vec<usize>;
+    let mut gears2: Vec<usize> = vec![];
+    for line in lines {
+        if let Ok(txt) = line {
+            let (int_pos, gear_pos) = parse_line(&txt);
+            line0 = line1;
+            line1 = line2;
+            line2 = txt.to_string();
+            ints0 = ints1;
+            ints1 = ints2;
+            ints2 = Some(int_pos);
+            gears1 = gears2;
+            gears2 = gear_pos;
+            sum = sum + get_supergear_ratio(&gears1, &ints0, &ints1, &ints2, &line0, &line1, &line2);
+        }
+    }
+    line0 = line1;
+    line1 = line2;
+    line2 = String::from("");
+    ints0 = ints1;
+    ints1 = ints2;
+    ints2 = None;
+    gears1 = gears2;
+    sum = sum + get_supergear_ratio(&gears1, &ints0, &ints1, &ints2, &line0, &line1, &line2);
+    println!("{}", sum);
+}
+
+fn parse_line(line: &str) -> (Vec<(usize, usize)>, Vec<usize>) {
+    let mut in_int = false;
+    let mut int_start: usize = 0;
+    let mut int_end: usize;
+    let mut gear_pos: Vec<usize> = vec![];
+    let mut int_pos: Vec<(usize, usize)> = vec![];
+    for c in line.char_indices() {
+        match c.1 {
+            '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => {
+                if !in_int {
+                    in_int = true;
+                    int_start = c.0;
+                }
+            }
+            '*' => {
+                if in_int {
+                    int_end = c.0;
+                    in_int = false;
+                    int_pos.push((int_start, int_end));
+                }
+                gear_pos.push(c.0);
+            },
+            _ => {
+                if in_int {
+                    int_end = c.0;
+                    in_int = false;
+                    int_pos.push((int_start, int_end));
+                }
+            },
+        }
+    };
+    if in_int {
+        int_end = line.len();
+        int_pos.push((int_start, int_end));
+    }
+    (int_pos, gear_pos)
+}
+
+
+fn get_supergear_ratio<'a>(gear_pos: &Vec<usize>, int_pos0: &'a Option<Vec<(usize, usize)>>, int_pos1: &'a Option<Vec<(usize, usize)>>, int_pos2: &'a Option<Vec<(usize, usize)>>, line0: &str, line1: &str, line2: &str) -> usize {
+    let mut ratio: usize = 0;
+    for gp in gear_pos.iter() {
+        ratio = ratio + get_ratio(*gp, int_pos0, int_pos1, int_pos2, line0, line1, line2);
+    }
+    ratio
+}
+
+fn get_ratio<'a>(gear_pos: usize, int_pos0: &Option<Vec<(usize, usize)>>, int_pos1: &'a Option<Vec<(usize, usize)>>, int_pos2: &'a Option<Vec<(usize, usize)>>, line0: &str, line1: &str, line2: &str) -> usize {
+    let mut adj_gears: Vec<usize> = vec![];
+    for ip0 in int_pos0.clone().unwrap_or_default() {
+        if ip0.0.checked_sub(1).unwrap_or_default() <= gear_pos && gear_pos <= ip0.1 {
+            let num = line0.get(ip0.0..ip0.1).unwrap().parse::<usize>().unwrap();
+            adj_gears.push(num);
+        }
+    }
+    for ip1 in int_pos1.clone().unwrap_or_default() {
+        if ip1.0.checked_sub(1).unwrap_or_default() <= gear_pos && gear_pos <= ip1.1 {
+            let num = line1.get(ip1.0..ip1.1).unwrap().parse::<usize>().unwrap();
+            adj_gears.push(num);
+        }
+    }
+    for ip2 in int_pos2.clone().unwrap_or_default() {
+        if ip2.0.checked_sub(1).unwrap_or_default() <= gear_pos && gear_pos <= ip2.1 {
+            let num = line2.get(ip2.0..ip2.1).unwrap().parse::<usize>().unwrap();
+            adj_gears.push(num);
+        }
+    }
+    println!("Gear {}:", gear_pos);
+    println!("l0 {}:", line0);
+    println!("l1 {}:", line1);
+    println!("l2 {}:", line2);
+    for g in adj_gears.iter() {
+        println!("{}", g);
+    }
+    if adj_gears.len() == 2 {
+        return adj_gears[0] * adj_gears[1];
+    }
+    0
+}
+
+fn read_lines(filename: &str) -> io::Result<Lines<BufReader<File>>> {
+    let f = File::open(filename)?;
+    Ok(BufReader::new(f).lines())
+}
